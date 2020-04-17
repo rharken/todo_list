16f81aeb11ff53ec08faf692769e5b4fce470df2
Richard Harken <theharkens@charter.net>
2020-04-17 00:07:02 -0400

    Added new view FT passes

--- lists/tests.py
+++ lists/tests.py
@@ -40,21 +40,36 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
 
+
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.lists.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
 
-    def test_displays_all_items(self):
-        list_ = List.lists.create()
-        Item.items.create(text='itemey 1', list=list_)
-        Item.items.create(text='itemey 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.lists.create()
+        Item.items.create(text='itemey 1', list=correct_list)
+        Item.items.create(text='itemey 2', list=correct_list)
+        other_list = List.lists.create()
+        Item.items.create(text='other list item 1', list=other_list)
+        Item.items.create(text='other list item 2', list=other_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.lists.create()
+        correct_list = List.lists.create()
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+
+        self.assertEqual(response.context['list'], correct_list)
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
@@ -65,4 +80,32 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text' : 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.lists.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.lists.create()
+        correct_list = List.lists.create()
+
+        self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text' : 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.items.count(), 1)
+        new_item = Item.items.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.lists.create()
+        correct_list = List.lists.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text' : 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -6,11 +6,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.items.all()    
-    return render(request, 'list.html', {'items' : items})
+def view_list(request, list_id):
+    list_ = List.lists.get(id=list_id)
+    return render(request, 'list.html', {'list' : list_})
 
 def new_list(request):
-        list_ = List.lists.create()
-        Item.items.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/the-only-list-in-the-world/')
+    list_ = List.lists.create()
+    Item.items.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.lists.get(id=list_id)
+    Item.items.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
