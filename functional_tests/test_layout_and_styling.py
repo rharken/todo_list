ae3d1bd390463d634e001b9db5e5783e0404d258
Richard Harken <theharkens@charter.net>
2020-04-20 21:43:36 -0400

    Moved Fts into their own individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from unittest import skip
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    def test_layout_and_styling(self):
+        x_resolution = 1024
+        y_resolution = 768
+        # Edit goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(x_resolution, y_resolution)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            x_resolution / 2,
+            delta=10
+        )
+
+        # She starts a new list and sees the input
+        # is nicely centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            x_resolution / 2,
+            delta=10
+        )
