3fa4c29dde3dd629057cfb06eeab376598627f9e
Richard Harken <theharkens@charter.net>
2020-05-10 12:26:16 -0400

    Changes to deployment file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -32,8 +32,12 @@ def _create_or_update_dotenv():
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
-        new_secret = ''.join(random.SystemRandom().choices( 'abcdefghijklmnopqrstuvwxyz0123456789', k=50))
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        email_password = os.environ['EMAIL_PASSWORD']
+        append('.env', f'EMAIL_PASSWORD={email_password}')
 
 def _update_static_files():
     run('./venv/bin/python3.8 manage.py collectstatic --noinput')
