b0e970244bed3d3e0faec07758844ccfdd02c78c
Richard Harken <theharkens@charter.net>
2020-05-10 02:09:41 -0400

    environment differences

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -2,7 +2,7 @@ from fabric.api import run
 from fabric.context_managers import settings, shell_env
 
 def _get_manage_dot_py(host):
-    return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/manage.py'
+    return f'~/sites/{host}/venv/bin/python ~/sites/{host}/manage.py'
 
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
