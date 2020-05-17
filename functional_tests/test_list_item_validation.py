8db08cc08dd8c7c8475ee014dd60c29bd54aefa3
Richard Harken <theharkens@charter.net>
2020-05-17 00:15:41 -0400

    add screenshot on failure to FT runner

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -47,7 +47,6 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
-
     def test_cannot_add_duplicate_items(self):
         # Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
